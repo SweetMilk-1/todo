c0b62c37c2a0b885bc7e081954221b2eb68f7251
Andrey Smolyov <andreysmolyov@yandex.ru>
2022-12-21 17:00:37 +0800

    add props

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const TodoListItem = () => {
-    return <span>Drink Coffee</span>
+const TodoListItem = ({ label, important = false }) => {
+    const liStyle = {
+        color: important ? 'tomato' : 'black'
+    }
+    return <span style={liStyle}>{ label }</span>
 }
 
 export default TodoListItem
