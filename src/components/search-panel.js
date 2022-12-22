9bae22de40cef81aebb8877632456912c20b7cd2
Andrey Smolyov <andreysmolyov@yandex.ru>
2022-12-22 10:40:59 +0800

    landing

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,7 +1,24 @@
 import React from "react";
+import './search-panel.css'
 
 const SearchPanel = () => {
-    return <input placeholder="search" />
+    return (
+      <div className="row search-panel">
+        <div className="col-md-6 margin-top-10" >
+          <input type="text" className="form-control search-panel-input-text" id="inputText" placeholder="Search"/>
+        </div>
+        <div className="col-md-6 margin-top-10 btn-group" role="group">
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" defaultChecked/>
+            <label className="btn btn-outline-primary" htmlFor="btnradio1">All</label>
+          
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"/>
+            <label className="btn btn-outline-primary" htmlFor="btnradio2">Active</label>
+
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"/>
+            <label className="btn btn-outline-primary" htmlFor="btnradio3">Done</label>
+        </div>
+      </div>
+    );
   }
 
 export default SearchPanel;
