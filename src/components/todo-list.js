c58814ccfce82eb521136affb7740a8cb7481649
Andrey Smolyov <andreysmolyov@yandex.ru>
2022-12-22 09:26:44 +0800

    before landing

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,18 +2,21 @@ import React from "react";
 import TodoListItem from './todo-list-item'
 
 const TodoList = ({ todos }) => {
+
     const elements = todos.map(( item )=>{
+      
+      const {id, ...itemProps } = item;
+
       return (
-        <li>
-          <TodoListItem
-            label={item.label}
-            important={item.important}/>
+        <li key={id}>
+          <TodoListItem { ...itemProps }/>
         </li>
       );
     });
+    
     return (
       <ul>
-        {elements}
+        { elements }
       </ul>
     );
   }
